a9e30fd68dbcc7f196d21f8a1aa1c3a51a91c174
Paulo Maia <santosmaia31@gmail.com>
2025-02-26 19:48:03 -0300

    Desafio 01

---
+++ Desafio01.js
@@ -0,0 +1,30 @@
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+if (numeroUm === stringUm) {
+  console.log('As variáveis numeroUm e stringUm têm o mesmo valor e mesmo tipo');
+} else if (numeroUm == stringUm) {
+  console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes');
+} else {
+  console.log('As variáveis numeroUm e stringUm não têm o mesmo valor');
+}
+
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta têm o mesmo valor e mesmo tipo');
+} else if (numeroTrinta == stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta têm o mesmo valor, mas tipos diferentes');
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não têm o mesmo valor');
+}
+
+if (numeroDez === stringDez) {
+  console.log('As variáveis numeroDez e stringDez têm o mesmo valor e mesmo tipo');
+} else if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez têm o mesmo valor, mas tipos diferentes');
+} else {
+  console.log('As variáveis numeroDez e stringDez não têm o mesmo valor');
+}
